20cff211e97bf48bdace1179052149f1f32b8329
Mahmoud Khatib <mik2116@columbia.edu>
2019-12-09 19:28:55 +0000

    edits

--- sightings/urls.py
+++ sightings/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
         path('map/',views.showmap, name = 'showmap'),
         path('sightings/add', views.add, name='add'),
         path('sightings/stats', views.stats, name='stats'),
-        path('sightings/sightings', views.sightings, name='sightings'),
+        path('sightings/', views.sightings, name='sightings'),
         path('sightings/<unique_squirrel_id>', views.update, name='update'),
     ]
 
