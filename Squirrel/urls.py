576cb329f6225315c5aab2435382376a10b1a5a3
Mahmoud Khatib <mik2116@columbia.edu>
2019-12-09 18:56:43 +0000

    urls.py edits (both)

--- Squirrel/urls.py
+++ Squirrel/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
 from sightings import views
 
--- sightings/urls.py
+++ sightings/urls.py
@@ -4,8 +4,13 @@ from django.conf.urls import include
 from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
         path('map/',views.showmap, name = 'showmap'),
+        path('sightings/add', views.add, name='add'),
+        path('sightings/stats', views.stats, name='stats'),
+        path('sightings/sightings', views.sightings, name='sightings'),
+        path('sightings/<unique_squirrel_id>', views.update, name='update'),
     ]
 
 
